0beee55104ba562eb3a427431f1b228625a228b7
PixelyIon <pixelyion@protonmail.com>
2021-11-20 22:02:00 +0530

    Implement yuzu `common` within license exemption constraints
    The shader compiler use a subset of yuzu's `common`, this contains a lot of GPL code that isn't covered under our exemptions so we needed to selectively reimplement parts of it alongside certain parts that just don't map to Skyline such as `Settings`.

---
+++ common/settings.h
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MPL-2.0
+// Copyright © 2021 Skyline Team and Contributors (https://github.com/skyline-emu/)
+
+#pragma once
+
+#include "common_types.h"
+
+namespace Shader {
+    struct Settings {
+        struct Values {
+            bool renderer_debug{};
+            bool disable_shader_loop_safety_checks{};
+            struct ResolutionScalingInfo {
+                u32 up_scale{1};
+                u32 down_shift{0};
+                f32 up_factor{1.0f};
+                f32 down_factor{1.0f};
+                bool active{};
+                bool downscale{};
+            } resolution_info{};
+        };
+
+        static Values values;
+    };
+}
