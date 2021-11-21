7f0456cf557eae8157b77bc2ef87889c1838816c
PixelyIon <pixelyion@protonmail.com>
2021-11-21 15:06:09 +0530

    Implement Skyline Proxy Settings
    Settings values are to be handled by proxy as well by Skyline code, it was determined to do things in this manner it was rather pointless to have the settings struct which has been now eliminated and been replaced with a namespace of the same name.

--- common/settings.h
+++ common/settings.h
@@ -5,21 +5,22 @@
 
 #include "common_types.h"
 
-namespace Shader {
-    struct Settings {
-        struct Values {
-            bool renderer_debug{};
-            bool disable_shader_loop_safety_checks{};
-            struct ResolutionScalingInfo {
-                u32 up_scale{1};
-                u32 down_shift{0};
-                f32 up_factor{1.0f};
-                f32 down_factor{1.0f};
-                bool active{};
-                bool downscale{};
-            } resolution_info{};
-        };
-
-        static Values values;
+namespace Shader::Settings {
+    /**
+     * @note Only contains the settings relevant to the shader compiler
+     */
+    struct Values {
+        bool renderer_debug;
+        bool disable_shader_loop_safety_checks;
+        struct ResolutionScalingInfo {
+            u32 up_scale{1};
+            u32 down_shift{0};
+            f32 up_factor{1.0f};
+            f32 down_factor{1.0f};
+            bool active{};
+            bool downscale{};
+        } resolution_info;
     };
+
+    static inline Values values{}; //!< A static structure with the values set by Skyline code
 }
