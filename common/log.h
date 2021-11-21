0e5be95466be9a3c42480bfaaaf2b4b3f05c55ab
PixelyIon <pixelyion@protonmail.com>
2021-11-21 15:05:49 +0530

    Implement Skyline Proxy Logger
    We define certain functions that will be implemented in Skyline to allow for zero-overhead interaction between them while allowing the shader compiler to directly use Skyline's Logger APIs.

--- common/log.h
+++ common/log.h
@@ -3,6 +3,15 @@
 
 #pragma once
 
-#define LOG_DEBUG(...)
-#define LOG_WARNING(...)
-#define LOG_ERROR(...)
+#include <fmt/format.h>
+
+namespace Shader::Log {
+    // Proxy logging framework, these functions are implemented by Skyline
+    void Debug(const std::string& message);
+    void Warn(const std::string& message);
+    void Error(const std::string& message);
+}
+
+#define LOG_DEBUG(tag, message, ...) Shader::Log::Debug(fmt::format("Shader Compiler (" #tag "): " message __VA_OPT__(,) __VA_ARGS__))
+#define LOG_WARNING(tag, message, ...) Shader::Log::Warn(fmt::format("Shader Compiler (" #tag "): " message __VA_OPT__(,) __VA_ARGS__))
+#define LOG_ERROR(tag, message, ...) Shader::Log::Error(fmt::format("Shader Compiler (" #tag "): " message __VA_OPT__(,) __VA_ARGS__))
